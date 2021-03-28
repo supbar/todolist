6fc1fa4e07abb72e33113bd407cdc19edf410929
Supbar <core.editor>
2021-03-28 20:53:38 +0800

    Compiled the app succefully..

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ require('./bootstrap');
 import Vue from 'vue';
 import App from './comps/app.vue';
 
-new Vue({
-    render: h => h(App)
-  })
+const app = new Vue({
+  el: '#app',
+  components: {
+    App
+  }
+})
